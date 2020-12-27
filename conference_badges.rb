6d66616c4542b1bdd092f11d2fd34423dcf1dda0
Luis Martinez <luis_mmartinez@live.com>
2020-12-27 05:45:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.collect {|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(attendees)
+  attendees.collect {|name| "Hello, #{name}! You'll be assigned to room #{attendees.index(name)+1}!"}
+end
+
+def printer(attendees)
