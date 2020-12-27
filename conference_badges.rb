f9539269ab77ddf043cf5b45341ce2f7ad2339d8
Luis Martinez <luis_mmartinez@live.com>
2020-12-27 06:06:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,9 +15,8 @@ end
 def printer(attendees)
   batch_badge_creator(attendees).each do |element|
     puts element
-
   end
-  assign_rooms(attendees). each do |element|
+  assign_rooms(attendees).each do |element|
     puts element
     #binding.pry
   end
